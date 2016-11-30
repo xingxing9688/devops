28bf5606920a90f8f4f669e2a7fbeeae504c02c3
xingxingwang <1218761836@qq.com>
2016-12-01 00:10:33 +0800

    debian

---
+++ debian/debian_install.sh
@@ -0,0 +1,16 @@
+#/bin/bash 
+#debian 安装添加源
+#修改vim /etc/apt/sources.list配置文件
+# 其中 stretch 是版本号
+deb http://ftp.be.debian.org/debian stretch main contrib non-free
+deb http://ftp.cn.debian.org/debian stretch main contrib non-free
+
+# 安装无线网卡
+
+sudo apt-get install fireware-* 
+
+
+
+
+
+
