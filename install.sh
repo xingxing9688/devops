a40e1fdc88ed539cecf5814fcb0fb37232246efb
xingxingwang <xingxingwang0122@gmail.com>
2017-02-13 22:16:15 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -26,20 +26,7 @@ docker_install()
   systemctl enable docker 
  }
 
-useradd()
-{
- useradd wangxingxing && usermod -G wheel wangxingxing   
- mkdir /home/wangxingxing/.ssh && chmod 700 /home/wangxingxing/.ssh 
- chown  wangxingxing:wangxingxing /home/wangxingxing/.ssh 
- wget https://raw.githubusercontent.com/xingxing9688/system/master/authorized_keys -P /home/wangxingxing/.ssh/
- chmod 644 /home/wangxingxing/.ssh/authorized_keys 
- chown wangxingxing:wangxingxing /home/wangxingxing/.ssh/authorized_keys
-}
 
-hostname()
- {  
-  echo "proapp1" > /etc/hostname 
- }
 
 #iptables -t nat -I POSTROUTING -s 192.168.8.0/24 -j SNAT --to-source 192.168.8.1  
 
@@ -48,9 +35,6 @@ yum_update
 
 sshd_config
 echo "sshd_config"
-#docker_install
-useradd 
-echo "useradd"
 
 
 
